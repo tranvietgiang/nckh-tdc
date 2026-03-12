1fd8791203145df28992b5dbc5ce9cebfd7bdbcc
tran viet giang <tranvietgiang25@gmail.com>
2026-03-13 00:23:48 +0700

    complete after logging in, user cannot go back to the login page

--- frontend-reactjs/src/routes/index.jsx
+++ frontend-reactjs/src/routes/index.jsx
@@ -1,12 +1,20 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "../pages/Auth/Login";
 import RoleRoutes from "./role.route";
+import GuestRoute from "./guest.route";
 
 function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<Login />} />
+        <Route
+          path="/login"
+          element={
+            <GuestRoute>
+              <Login />
+            </GuestRoute>
+          }
+        />
 
         <Route path="/*" element={<RoleRoutes />} />
       </Routes>
