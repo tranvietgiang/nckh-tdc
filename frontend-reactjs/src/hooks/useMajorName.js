def23a162959c2aaf80b01af77bb4fe9d5ab6f14
tran viet giang <tranvietgiang25@gmail.com>
2026-03-14 16:37:44 +0700

    Complete teacher information display

---
+++ frontend-reactjs/src/hooks/useMajorName.js
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+import { majorApi } from "../api";
+
+export default function useMajorName(majorId) {
+  const [majorName, setMajorName] = useState("");
+
+  useEffect(() => {
+    if (!majorId) return;
+
+    const fetchMajor = async () => {
+      try {
+
+        const res = await majorApi.getById(majorId);
+        setMajorName(res.major_name);
+      } catch (err) {
+        console.error(err);
+      } 
+    };
+
+    fetchMajor();
+  }, [majorId]);
+
+  return { majorName };
+}
